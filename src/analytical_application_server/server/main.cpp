699f2b5c26445de9a98c43163e5568c50bb66fca
Nikolai Ryzhov <dolaxom4@gmail.com>
2024-10-03 19:38:06 +0000

    [DAAS][Userver] Initing base message web handler by Userver

--- src/analytical_application_server/server/main.cpp
+++ src/analytical_application_server/server/main.cpp
@@ -1,5 +1,14 @@
+#include <userver/components/minimal_server_component_list.hpp>
+#include <userver/utest/using_namespace_userver.hpp>
+#include <userver/utils/daemon_run.hpp>
+ 
+#include <userver/utest/using_namespace_userver.hpp>
+ 
+#include "controllers/message_handler.hpp"
+ 
+int main(int argc, char* argv[]) {
+  auto component_list = components::MinimalServerComponentList()
+                            .Append<controllers::msg::MessageHandler>();
 
-
-int main() {
-
+  return utils::DaemonMain(argc, argv, component_list);
 }
