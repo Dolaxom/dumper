bda62823eac8ff5aeade924ff08bbe31a32d89a0
Nikolai <dolaxom4@gmail.com>
2024-10-05 01:27:58 +0300

    [DAAS][WIP] Adding serialization and deserialization for sheme structs in Model

---
+++ src/analytical_application_server/server/models/deserialization.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "schema.hpp"
+#include <string_view>
+
+namespace model {
+
+inline Server FromJson(std::string_view json) {
+    Server server_json;
+
+    // TODO IMPL
+
+    return server_json;
+}
+
+} // namespace model
---
+++ src/analytical_application_server/server/models/serialization.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "schema.hpp"
+
+namespace model {
+
+inline std::string ToJson(Server server) {
+    std::string json;
+
+    // TODO IMPL
+
+    return json;
+}
+
+
+} // namespace model
